1050bd2b90bd049218c4184e6ae43ca5ef099f31
fikretatessacan <fikretatessacan@gmail.com>
2026-03-02 05:26:51 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,21 @@
-const CACHE = 'f701-056-v1';
+const CACHE_NAME = 'tanik-denetim-v11';
+const urlsToCache = ['./', './index.html'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.add('/tanik-denetim-form/'))
-      .then(() => self.skipWaiting())
-  );
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)));
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
-    )).then(() => self.clients.claim())
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
   e.respondWith(
-    caches.match(e.request).then(cached => {
-      const network = fetch(e.request).then(res => {
-        if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
-        return res;
-      }).catch(() => cached);
-      return cached || network;
-    })
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
